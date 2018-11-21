1af95423e74bb3ba8e624962b399de96275e613e
Khatna Bold <bold@princeton.edu>
2018-11-21 04:49:54 +0000

    v1: project thumbnails, link transitions

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,7 @@
 // underscore flicker effect
-
-const no_  = "Mongolian student at Princeton University"
-const yes_ = "Mongolian student at Princeton University_"
-
-let underscore = false;
-
-function changeBio(underscore) {
-	if (underscore)
-		$("#bio").html(no_);
-	else
-		$("#bio").html(yes_);
-}
-
 setInterval(() => {
-	changeBio(underscore);
-	underscore = !underscore;
+	$("#_").toggleClass("hidden");
 }, 1000);
 
-// -----------------------------------------------------------------------------
+// Open new tabs ---------------------------------------------------------------
+$("a").attr("target", "_blank");
