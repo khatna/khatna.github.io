cd518c37b43266f70cd1d58590cd01f23d51f0af
khatna <khatna@github.com>
2020-01-28 01:41:53 -0500

    add project descriptions

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,17 @@
 // underscore flicker effect
-setInterval(() => {
+setInterval(function () {
 	$("#_").toggleClass("hidden");
 }, 1000);
+
+// hover shows text
+$(".project a").hover(
+	function () {
+		$($(this).children()[0]).css('-webkit-filter', 'brightness(20%)');
+		$($(this).children()[1]).css('display', 'inline');
+	}, function () {
+		$($(this).children()[0]).css('-webkit-filter', 'brightness(100%)');
+		$($(this).children()[1]).css('display', 'none');
+});
 
 // Open new tabs ---------------------------------------------------------------
 $("a").attr("target", "_blank");
